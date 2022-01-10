c2e7c1d57ef67c9e4f5388c6d0e9a623e5d8418e
Laxiflora <38elton@gmail.com>
2022-01-11 02:29:20 +0800

    Hw3.5_Fin!

--- Hw3.5/sniffing.c
+++ Hw3.5/sniffing.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 
 #include<string.h>
-
+#include <net/ethernet.h>
 #include<stdlib.h>
-
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <netinet/ip.h>
+#include <netinet/ether.h>
+#include <sys/socket.h>
+#include <linux/ipv6.h>
 #include <pcap.h>
 #include <pcap/pcap.h>
-
+#include <bits/endian.h>
+#include <arpa/inet.h>
 #include<netinet/in.h>
 
 #include<time.h>
@@ -16,9 +22,6 @@
 #define STRSIZE 1024
 
 void handle_packet(u_char* args,const struct pcap_pkthdr* header,const u_char* packet){
-	//figure out what protocol is using
-	struct ether_header* eth_header;
-	eth_header = (struct ether_header*) (packet);
 
     printf("---------------------PACKET RECEIVE-----------------------\n");
 
@@ -30,147 +33,69 @@ void handle_packet(u_char* args,const struct pcap_pkthdr* header,const u_char* p
        than what we currently have captured. If the snapshot
        length set with pcap_open_live() is too small, you may
        not have the whole packet. */
+
+	//figure out what protocol is using
+	
+    struct ether_header* eth_header;
+    eth_header = (struct ether_header*)packet;
+
     printf("Total packet available: %d bytes\n", header->caplen);
     printf("Expected packet size: %d bytes\n", header->len);
 
-
+    //print time
     struct tm *lt;
 	char timestr[80];
 	time_t local_tv_sec;
-
-	int size_ip;
-	int size_tcp;
-	int size_udp;
-	int size_payload;
-
 	local_tv_sec = header->ts.tv_sec;
 	lt = localtime(&local_tv_sec);
 	strftime(timestr, sizeof(timestr), "%b %d %Y, %X", lt);
     printf("Receive Time: %s\n",timestr);
-
-
+    //print MAC address
+    printf("Src MAC address : %02X : %02X : %02X : %02X : %02X : %02X\n" , eth_header->ether_shost[0],eth_header->ether_shost[1],eth_header->ether_shost[2],eth_header->ether_shost[3],eth_header->ether_shost[4],eth_header->ether_shost[5]);
+    printf("Dst MAC address : %02x : %02x : %02x : %02x : %02x : %02x\n" , eth_header->ether_dhost[0],eth_header->ether_dhost[1],eth_header->ether_dhost[2],eth_header->ether_dhost[3],eth_header->ether_dhost[4],eth_header->ether_dhost[5]);
+    printf("Ether Type = 0x%04x\n" , eth_header->ether_type);
     /* Pointers to start point of various headers */
-	const u_char *ip_header;
-    const u_char *tcp_header;
-    const u_char *payload;
+    struct iphdr* ipv4_header;
+    struct ipv6hdr* ipv6_header;
+    struct tcphdr* tcp_header;
+    struct udphdr* udp_header;
 
     /* Header lengths in bytes */
-    int ethernet_header_length = 14; /* Doesn't change */
-    int ip_header_length;
-    int tcp_header_length;
-    int payload_length;
-
-	ip_header = packet;
-	/* Get MAC Address */
-	printf("Dest Mac Address :");
-	for(int i=0;i<4;i++,ip_header++){
-		printf("%02x ",*ip_header);
-	}
-	printf("\nSrc Mac Address : ");
-	for(int i=0;i<4;i++,ip_header++){
-		printf("%02x ",*ip_header);
-	}
-    printf("\nEthernet Type : 0x");
-    for(int i=0;i<2;i++,ip_header++){
-        printf("%02x",*ip_header);
-    }
-
-
-    /* Find start of IP header */
-    ip_header = packet + ethernet_header_length;
-    /* The second-half of the first byte in ip_header
-       contains the IP header length (IHL). */
-    ip_header_length = ((*ip_header) & 0x0F);
-    /* The IHL is number of 32-bit segments. Multiply
-       by four to get a byte count for pointer arithmetic */
-    ip_header_length = ip_header_length * 4;
-	//process IP header
-	if(ip_header_length<20){
-		printf("Invalid IP header!\n");
-		return;
-	}
-	else{
-		printf("\nIP header length (IHL) in bytes: %d", ip_header_length);
-        ip_header = ip_header + 12;
-        printf("\nSrc IP address : 0x");
-        for(int i=0;i<4;i++,ip_header++){
-            printf("%02x",*ip_header);
+	if(eth_header->ether_type == ETHERTYPE_IP || ETHERTYPE_IPV6){  //is a IP packet
+        ipv4_header = (struct iphdr*)(packet + sizeof(struct ether_header));
+        if(ipv4_header->version == 6){  //is a IPV6 packet
+            ipv6_header = (struct ipv6hdr*)(packet + sizeof(struct ether_header));
+            printf("It's a IPv6 packet.\n");
+            printf("Src IP = ");
+            for(int i=0;i<16;i+=2){
+                printf("%2x%2x:",ipv6_header->saddr.s6_addr[i],ipv6_header->saddr.s6_addr[i+1]);
+            }
+            printf("\nDest IP =");
+            for(int i=0;i<16;i+=2){
+                printf("%2x%2x:",ipv6_header->daddr.s6_addr[i],ipv6_header->daddr.s6_addr[i+1]);
+            }
         }
-        printf("\nDest IP address : 0x");
-        for(int i=0;i<4;i++,ip_header++){
-		    printf("%02x",*ip_header);
-	    }
-	}
-    
-
-    /* Now that we know where the IP header is, we can 
-       inspect the IP header for a protocol number to 
-       make sure it is TCP before going any further. 
-       Protocol is always the 10th byte of the IP header */
-    ip_header = packet + ethernet_header_length;
-    u_char protocol = *(ip_header + 9);  //+1 -> +1byte
-    printf("\nProtocol Type(in hex) : %02x",protocol);
-    if (protocol != IPPROTO_TCP && protocol != IPPROTO_UDP) {
-        printf("\nNot a TCP/UDP packet.\n\n");
-        return;
-    }
-    else if(protocol == IPPROTO_TCP){
-        /* process TCP package */
-        tcp_header = packet + ethernet_header_length + ip_header_length;
-        printf("\nIs TCP , src port = 0x%02x" , *tcp_header++);
-        printf(" %02x",*tcp_header++);
-        printf("\nDes port = %02x" , *tcp_header++);
-        printf(" %02x",*tcp_header++);
-    }
-    else{
-        printf("\nIs UDP , src port = 0x%02x" , *tcp_header++);
-        printf("%02x",*tcp_header++);
-        printf("\nDes port = 0x%02x" , *tcp_header++);
-        printf("%02x",*tcp_header++);
-    }
-
-    /* Add the ethernet and ip header length to the start of the packet
-       to find the beginning of the TCP header */
-    tcp_header = packet + ethernet_header_length + ip_header_length;
-    /* TCP header length is stored in the first half 
-       of the 12th byte in the TCP header. Because we only want
-       the value of the top half of the byte, we have to shift it
-       down to the bottom half otherwise it is using the most 
-       significant bits instead of the least significant bits */
-    tcp_header_length = ((*(tcp_header + 12)) & 0xF0) >> 4;
-    
-    
-    /* The TCP header length stored in those 4 bits represents
-       how many 32-bit words there are in the header, just like
-       the IP header length. We multiply by four again to get a
-       byte count. */
-    //tcp_header_length = tcp_header_length * 4;
-    //printf("\nTCP header length in bytes: %d\n", tcp_header_length);
-
-
-
-    /* Add up all the header sizes to find the payload offset */
-    int total_headers_size = ethernet_header_length+ip_header_length+tcp_header_length;
-    printf("\nSize of all headers combined: %d bytes\n", total_headers_size);
-    payload_length = header->caplen -
-        (ethernet_header_length + ip_header_length + tcp_header_length);
-    printf("Payload size: %d bytes\n", payload_length);
-    payload = packet + total_headers_size;
-    printf("Memory address where payload begins: %p\n\n", payload);
-
-
-    /* Print payload in ASCII */
-    /*  
-    if (payload_length > 0) {
-        const u_char *temp_pointer = payload;
-        int byte_count = 0;
-        while (byte_count++ < payload_length) {
-            printf("%c", *temp_pointer);
-            temp_pointer++;
+        else{
+            printf("It's a IPV4 packet.\n");
+            printf("Src addr = %s\n", inet_ntoa(*(struct in_addr*) &ipv4_header->saddr) );
+            printf("Dst addr = %s\n", inet_ntoa(*(struct in_addr*) &ipv4_header->daddr) );
         }
-        printf("\n");
+
+        if(ipv4_header->protocol == IPPROTO_TCP){ //is a TCP protocol
+            tcp_header = (struct tcphdr*)(packet+sizeof(struct ether_header)+sizeof(struct iphdr));
+            printf("It's a TCP packet.\n");
+            printf("Src Port = %4X\n" , tcp_header->source);
+            printf("Dst Port = %4X\n" , tcp_header->dest);
+        }
+        else if(ipv4_header->protocol == IPPROTO_UDP){ //is a UDP protocol
+            udp_header = (struct udphdr*)(packet+sizeof(struct ether_header)+sizeof(struct iphdr));
+            printf("It's a UDP packet.\n");
+            printf("Src Port = %4X\n" , udp_header->source);
+            printf("Dst Port = %4X\n" , udp_header->dest);
+        }
+
+
     }
-    */
 
     return;
 }
